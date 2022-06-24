e9b057e6af8a6794ad2cb72c6b92079d96f68515
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-24 15:47:09 +0300

    Add project files.

---
+++ CSharp11/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Metodlar ile ilgili bir örnek");
+EkranaYaz();
+Console.WriteLine("Toplam metodu çalıştı ve sonuç = " + Toplam(3, 2));
+
+
+
+static void EkranaYaz()
+{
+    Console.WriteLine("EkranaYaz metodu çalıştı...");
+}
+
+static int Toplam(int a, int b)
+{
+    return a+ b;
+}
